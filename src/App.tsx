0eaac326363671788ba094054a93806ad0f034dd
Valentin Madrid <valentin@incility.com>
2022-02-08 13:17:44 +0100

    📕 Create basic file
    Creating a little file to make some tests again.

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>What's your wallet adress</h1>
+        <input></input>
+
+        <h1>What's your business's name?</h1>
+        <input></input>
+
+        <h1>How much do you want to charge</h1>
+        <input></input>
       </header>
     </div>
   );
