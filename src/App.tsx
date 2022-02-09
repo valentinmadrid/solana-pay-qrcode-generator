ebec60fad7ed600df779ddab63e89eb0dfef223d
Valentin Madrid <valentin@incility.com>
2022-02-09 18:43:43 +0100

    😀 Off to a good start
    Start by doing some basic stuff and rendering a QR Code on a React Page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
+import { Cluster, clusterApiUrl, Connection, PublicKey, Keypair } from '@solana/web3.js';
+import {encodeURL, createQR} from '@solana/pay'
+import BigNumber from 'bignumber.js'
+
+let wallet="";
+let amount="";
+let token="";
 
 function App() {
+
+  async function main() {
+      // Variable to keep state of the payment status
+      let paymentStatus: string;
+  
+      // Connecting to devnet for this example
+      console.log('1. ✅ Establish connection to the network');
+      const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+  }
+
+  console.log('2. 🛍 Simulate a customer checkout \n');
+const amount = new BigNumber(1);
+const reference = new Keypair().publicKey;
+const label = 'Jungle Cats store';
+const message = 'Jungle Cats store - your order - #001234';
+const memo = 'JC#4098';
+const recipient = new PublicKey("CuieVDEDtLo7FypA9SbLM9saXFdb1dsshEkyErMqkRQq")
+console.log('3. 💰 Create a payment request link \n');
+const url = encodeURL({ recipient: recipient, amount, reference, label, message, memo });
+const qrCode = createQR(url, 1024, "white", "black" );
+const element = document.getElementById('qrcode')!;
+
+qrCode.append(element)
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{url}</h1>
         <h1>What's your wallet adress</h1>
+        
         <input></input>
-
+<div id='qrCode'>{qrCode.append}</div>
         <h1>What's your business's name?</h1>
         <input></input>
-
         <h1>How much do you want to charge</h1>
         <input></input>
+
+        <h1>Submit</h1>
+        <button>Generate QR Code</button>
       </header>
     </div>
   );
 }
 
+
 export default App;
+
+
