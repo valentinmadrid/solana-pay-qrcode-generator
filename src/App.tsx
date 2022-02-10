3f85d09251519f37ef82029c9484021edaad5a54
Valentin Madrid <valentin@incility.com>
2022-02-10 19:11:37 +0100

    🛠️ Make some changes to adjust
    Adjusting for separate QRCode Element

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,32 @@
 import React, { FC, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 import { Cluster, clusterApiUrl, Connection, PublicKey, Keypair } from '@solana/web3.js';
-import {encodeURL, createQR} from '@solana/pay'
-import BigNumber from 'bignumber.js'
 import QRCode from "./QRCode"
+import config from "../config.json"
 
 
-
-
-
-let wallet="";
-let amount="";
-let token="";
+process.env.REFERENCE = config.reference || ``;
+process.env.LABEL = config.label || ``;
+process.env.MESSAGE = config.message  || ``;
+process.env.MEMO = config.memo || ``;
+process.env.RECIPIENT = config.recipient || ``;
 
 function App() {
 
   async function main() {
-      // Variable to keep state of the payment status
       let paymentStatus: string;
-  
-      // Connecting to devnet for this example
       console.log('1. ✅ Establish connection to the network');
       const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
   }
 
-  console.log('2. 🛍 Simulate a customer checkout \n');
-const amount = new BigNumber(1);
-const reference = new Keypair().publicKey;
-const label = 'Jungle Cats store';
-const message = 'Jungle Cats store - your order - #001234';
-const memo = 'JC#4098';
-const recipient = new PublicKey("CuieVDEDtLo7FypA9SbLM9saXFdb1dsshEkyErMqkRQq")
-console.log('3. 💰 Create a payment request link \n');
-const url = encodeURL({ recipient: recipient, amount, reference, label, message, memo });
-const qrCode = createQR(url, 1024, "white", "black" );
-const element = document.getElementById('qrcode')!;
 
-qrCode.append(element)
+
+
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
