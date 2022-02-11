71599470a2e6f09c3df9518d4fbfdf297bd61faa
Valentin Madrid <valentin@incility.com>
2022-02-11 16:41:44 +0100

    🪢 Add React Router
    Add the React Router to be able to make "dynamic" pages.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ import React, { FC, useEffect, useLayoutEffect, useMemo, useRef, useState } from
 import './App.css';
 import { Cluster, clusterApiUrl, Connection, PublicKey, Keypair } from '@solana/web3.js';
 import QRCode from "./QRCode"
+import Sidebar from "./Sidebar"
 import config from "./config.json"
+import styled from "styled-components";
+import scrollreveal from "scrollreveal";
+import Dashboard from "./Dashboard"
+import RightSidebar from './RightSidebar';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
 
 process.env.REFERENCE = config.reference || ``;
@@ -11,7 +22,40 @@ process.env.MESSAGE = config.message  || ``;
 process.env.MEMO = config.memo || ``;
 process.env.RECIPIENT = config.recipient || ``;
 
+
+
 function App() {
+
+  useEffect(() => {
+    const sr = scrollreveal({
+      origin: "left",
+      distance: "80px",
+      duration: 1000,
+      reset: false,
+    });
+    sr.reveal(
+      `
+       #sidebar
+    `,
+      {
+        opacity: 0,
+      }
+    );
+    const sr2 = scrollreveal({
+      origin: "right",
+      distance: "80px",
+      duration: 1000,
+      reset: false,
+    });
+    sr2.reveal(
+      `
+       #rightSidebar
+    `,
+      {
+        opacity: 0,
+      }
+    );
+  }, []);
 
   async function main() {
       let paymentStatus: string;
@@ -21,14 +65,31 @@ function App() {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <button>Generate QR Code</button>
-        <QRCode />
-      </header>
-    </div>
+<Div>
+  <Router>
+    <Sidebar />
+    <Routes>
+      <Route path='/dashboard' element={<Dashboard />}>
+      </Route>
+    </Routes>
+    <RightSidebar />
+  </Router>
+</Div>
+
+
   );
 }
+
+const Div = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 12fr 4fr;
+  min-height: 100vh;
+  height: max-content;
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    grid-template-columns: 1fr;
+    height: max-content;
+  }
+`;
 
 
 export default App;
