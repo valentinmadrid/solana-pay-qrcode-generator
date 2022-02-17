86b52e72a97eb998e200a1db019c59912d7bdd5c
Valentin Madrid <valentin@incility.com>
2022-02-17 21:46:22 +0100

    🎈 Initialize NextJS

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import styled from "styled-components"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/hello.js
@@ -0,0 +1,6 @@
+const Hello = () => {
+    return(
+        <h1>hi</h1>
+    )
+}
+export default Hello;
