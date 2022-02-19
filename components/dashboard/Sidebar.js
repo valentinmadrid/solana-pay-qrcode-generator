28f95d3e67f0eaa4979a9df40f833786aa07a030
Valentin Madrid <valentin@incility.com>
2022-02-20 00:33:54 +0100

    ✨ - Update Sidebar for styling

--- components/dashboard/Sidebar.js
+++ components/dashboard/Sidebar.js
@@ -5,37 +5,37 @@ import styles from '../../styles/Sidebar.module.css'
 const Sidebar = () => {
     return (
         <aside>
-            <div className='sideBarTop'>
-                <div className='logo'>
+            <div className={styles.sideBarTop}>
+                <div className={styles.logo}>
                     <img src='https://img.search.brave.com/B7-CY8jOkJ0wlDhsF0NSh121BMkPPElLhuhOqJc58B4/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5K/LWZsRGZ1UHV1Uk80/X3o0QTlmc2dnSGFI/YSZwaWQ9QXBp'></img>
                 </div>
                 <div className={styles.closeSideBar}>
                     <CloseIcon />
                 </div>
             </div>
-            <div className='sideBar'>
-                <a href='#'>
-                    <DashboardSharp />
+            <div className={styles.sideBar}>
+                <a href='#' className={styles.active}>
+                    <DashboardSharp className='navIcon' />
                     <h3>Dashboard</h3>
                 </a>
                 <a href='#'>
-                    <DashboardSharp />
+                    <DashboardSharp className='navIcon' />
                     <h3>Customers</h3>
                 </a>
                 <a href='#'>
-                    <DashboardSharp />
+                    <DashboardSharp className='navIcon' />
                     <h3>Transactions</h3>
                 </a>
                 <a href='#'>
-                    <DashboardSharp />
+                    <DashboardSharp className='navIcon' />
                     <h3>Settings</h3>
                 </a>
                 <a href='#'>
-                    <DashboardSharp />
+                    <DashboardSharp className='navIcon' />
                     <h3>Dashboard</h3>
                 </a>
                 <a href='#'>
-                    <DashboardSharp />
+                    <DashboardSharp className='navIcon' />
                     <h3>Dashboard</h3>
                 </a>
             </div>
