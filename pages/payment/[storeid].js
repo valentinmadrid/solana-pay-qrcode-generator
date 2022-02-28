232ef5e4d19c094c3e221d995651da8aa03214fc
Valentin Madrid <valentin@incility.com>
2022-02-28 23:11:44 +0100

    🔧 - Make a Test for Dynamic Routing

--- pages/payment/[storeid].js
+++ pages/payment/[storeid].js
@@ -1,10 +1,55 @@
 import { useRouter } from "next/router";
-
+import { supabase } from '../../client'
+import { useEffect, useState } from "react";
 const Payment = () => {
+    const [loading, setLoading] = useState(true)
+    const [wallet, setWallet] = useState('')
+    const [url, setUrl] = useState('')
+    const [storeName, setStoreName] = useState('')
+    const [description, setDescription] = useState('')
     const router = useRouter()
     const storeid = router.query.storeid
+    useEffect(() => {
+        fetchData()
+        console.log(storeid)
+      }, [storeid])
+    
+      const user = supabase.auth.user()
+    
+      const fetchData = async() => {
+        setLoading(true)
+    
+    
+        let { data, error, status } = await supabase
+        .from('stores')
+        .select(`wallet, name`, `description`)
+        .eq('url', storeid)
+        .single()
+    
+      if (error && status !== 406) {
+        throw error
+        console.log(error)
+      }
+    
+      if (data) {
+        setWallet(data.wallet)
+        setUrl(data.url)
+        setStoreName(data.name)
+        setDescription(data.description)
+        setLoading(false)
+      }
+    
+      
+}
+
+
+
     return (
+        <div>
         <h1>Payment {storeid}</h1>
+        <h3>{storeName}</h3>
+        <h3>{description}</h3>
+        </div>
     )
 }
 
