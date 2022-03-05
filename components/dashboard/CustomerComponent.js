f5a5ff665f63a6a766cb146a770c75a9a2d3ed9d
Valentin Madrid <valentin@incility.com>
2022-03-05 23:06:33 +0100

    🔧 - Create Customers Component

---
+++ components/dashboard/CustomerComponent.js
@@ -0,0 +1,71 @@
+import styles from '../../styles/Home.module.css'
+import MoneyIcon from '@material-ui/icons/Money';
+import { useEffect, useState } from 'react';
+import { supabase } from '../../client';
+import Transactions from '../../pages/transactions';
+
+const CustomerComponent = () => {
+  const [loading, setLoading] = useState(true)
+  const [customers, setCustomers] = useState([])
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const user = supabase.auth.user()
+
+  const fetchData = async() => {
+    setLoading(true)
+    let { data, error, status } = await supabase
+    .from('customers')
+    .select(`user, totalpaid`, `userid`)
+    .eq('userid', user.id)
+    console.log(data)
+
+
+
+  if (error && status !== 406) {
+    throw error
+    console.log(error)
+  }
+
+  if (data) {
+    setCustomers(data)
+    console.log(data)
+    setLoading(false)
+  }
+
+  }
+
+    return (
+<main>
+  <h1>Dashoard</h1>
+  {/*---------- END OF INSIGHTS ------------*/}
+  <div className={styles.recentorders}>
+    <h2>Your Customers</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Wallet Adress</th>
+          <th>Total Amount Paid</th>
+
+        </tr>
+      </thead>
+      <tbody>
+          
+        {
+            customers.map((customers) => (
+                <tr className="tr" key={customers.id}>
+                             <td>{customers.wallet}</td>
+                             <td>{customers.totalpaid}</td>
+                         </tr>
+                ))}
+      </tbody>
+    </table>
+    <a href="#">Show All</a>
+  </div>
+</main>
+    )
+}
+
+export default CustomerComponent;
