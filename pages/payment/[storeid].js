c41c98379876682ac59c0188bfe58b666ee72bf3
Valentin Madrid <valentin@incility.com>
2022-03-05 22:37:24 +0100

    🔧 - First Attempt in generating a QR Code

--- pages/payment/[storeid].js
+++ pages/payment/[storeid].js
@@ -2,12 +2,14 @@ import { useRouter } from "next/router";
 import { supabase } from '../../client'
 import { useEffect, useState } from "react";
 import Modal from "react-modal"
-import { Cluster, clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
+import { Cluster, clusterApiUrl, Connection, PublicKey, Keypair } from '@solana/web3.js';
 import { encodeURL, createQR } from '@solana/pay';
 import BigNumber from 'bignumber.js';
+import { useRef, useLayoutEffect } from "react";
+
 const Payment = () => {
     const [loading, setLoading] = useState(true)
-    const [wallet, setWallet] = useState('')
+    const [wallet, setWallet] = useState('va1yPZsd2qieP5pE6gtxvAHkHKEW3qmtoZy3oN1GcBX')
     const [url, setUrl] = useState('')
     const [storeName, setStoreName] = useState('')
     const [description, setDescription] = useState('')
@@ -17,9 +19,10 @@ const Payment = () => {
     const [transactionDescription, setTransactionDescription] = useState('')
     const [amount, setAmount] = useState(1)
     const [modalIsOpen, setModalIsOpen] = useState(false)
+    const ref = useRef(null)
+    const [wallet1, setWallet1] = useState(new PublicKey(''))
 
     async function connect() {
-        const [paymentStatus, setPaymentStatus] = useState('')
         const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
     }
 
@@ -37,7 +40,7 @@ const Payment = () => {
     
         let { data, error, status } = await supabase
         .from('stores')
-        .select(`wallet, name`, `description`)
+        .select(`wallet, name`, `description`, `id`)
         .eq('url', storeid)
         .single()
     
@@ -47,6 +50,7 @@ const Payment = () => {
       }
     
       if (data) {
+        console.log(data.wallet)
         setWallet(data.wallet)
         setUrl(data.url)
         setStoreName(data.name)
@@ -56,6 +60,35 @@ const Payment = () => {
     
       
 }
+
+const generateQr = () => {
+
+}
+
+useEffect(() => {
+        const recipient = new PublicKey(wallet);
+        const transactionamount = new BigNumber(amount);
+        const reference = new Keypair().publicKey;
+        const label = storeName;
+        const message = transactionDescription;
+        const memo = storeName + "#";
+        
+        if (currency === 'SOL') {
+            let splToken = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
+        } else if (currency === 'USDC') {
+            let splToken = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
+        } else if (currency === 'ETH') {
+            let splToken = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
+        } else if (currency === 'BTC') {
+            let splToken = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
+        }
+        
+        setModalIsOpen(true)
+        const qrUrl = encodeURL({ recipient, transactionamount, splToken, reference, label, message, memo });
+        const qrCode = createQR(url);
+    qrCode?.append(ref.current)
+  }, [generateQr])
+
 
 
 
@@ -84,9 +117,10 @@ const Payment = () => {
             setTransactionDescription(e.target.value)
         }}
         />
-        <button onClick={() => setModalIsOpen(true)}>Create QR Code</button>
+        <button onClick={generateQr}>Create QR Code</button>
         <Modal isOpen={modalIsOpen}>
 <h1>hello</h1>
+<div ref={ref} />
         </Modal>
         </div>
     )
