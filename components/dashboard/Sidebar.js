7e6a780af0e66af0c5bff087e859845731c8220b
Valentin Madrid <valentin@incility.com>
2022-03-18 17:06:38 +0100

    🔧 - Use Link instead of href

--- components/dashboard/Sidebar.js
+++ components/dashboard/Sidebar.js
@@ -2,6 +2,7 @@ import CloseIcon from '@material-ui/icons/Close';
 import DashboardSharp from '@material-ui/icons/DashboardSharp';
 import styles from '../../styles/Sidebar.module.css'
 import { useRouter } from "next/router";
+import Link from 'next/link'
 
 const Sidebar = () => {
   const router = useRouter();
@@ -12,34 +13,34 @@ const Sidebar = () => {
 
   </div>
   <div className={styles.sidebar}>
-    <a href="/dashboard">
+    <Link href="/dashboard">
       <DashboardSharp />
       <h3>Dashboard</h3>
-    </a>
-    <a href="/settings">
+    </Link>
+    <Link href="/settings">
       <DashboardSharp />
       <h3>Settings</h3>
-    </a>
-    <a href="/transactions">
+    </Link>
+    <Link href="/transactions">
       <DashboardSharp />
       <h3>Transactions</h3>
-    </a>
-    <a href="/customers">
+    </Link>
+    <Link href="/customers">
       <DashboardSharp />
       <h3>Customers</h3>
-    </a>
-    <a href="/profile">
+    </Link>
+    <Link href="/profile">
       <DashboardSharp />
       <h3>Profile</h3>
-    </a>
-    <a href="/payment/cheezychips">
+    </Link>
+    <Link href="/payment/cheezychips">
       <DashboardSharp />
       <h3>My Store</h3>
-    </a>
-    <a href="#">
+    </Link>
+    <Link href="#">
       <DashboardSharp />
       <h3>Logout</h3>
-    </a>
+    </Link>
   </div>
 </aside>
 
