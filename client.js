c046802a9761ddbefbcb1c84fff182e744fea3b1
Valentin Madrid <valentin@incility.com>
2022-03-18 18:10:52 +0100

    ⚙️ - Update env again

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database 
-const supabase = createClient(env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_KEY)
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY)
 
 export { supabase }
