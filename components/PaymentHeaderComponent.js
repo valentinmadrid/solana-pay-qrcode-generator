19e60932f33046289a75ebd6d5624086e0a5be00
Valentin Madrid <valentin@incility.com>
2022-03-18 16:27:52 +0100

    🔧 - Add Props for Store Name

--- components/PaymentHeaderComponent.js
+++ components/PaymentHeaderComponent.js
@@ -1,12 +1,12 @@
 import styles from '../styles/Payment.module.css'
-export default function Header() {
+export default function Header(props) {
     return (
         <header className={styles.header}>
             <img 
                 src="./images/troll-face.png" 
                 className={styles.headerimage}
             />
-            <h2 className={styles.headertitle}>My cool store</h2>
+            <h2 className={styles.headertitle}>{props.storename}</h2>
             <h4 className={styles.headerproject}>Powered by Solana Pay</h4>
         </header>
     )
