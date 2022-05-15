5b66570212459ec3d3e238d9b63fea9a958bfba6
Valentin Madrid <valentin@incility.com>
2022-05-15 19:55:43 +0200

    🏜️ - Create Next App

--- pages/_app.js
+++ pages/_app.js
@@ -1,58 +1,7 @@
 import '../styles/globals.css'
-import { useState, useEffect } from 'react'
-import Link from 'next/link'
-import { supabase } from '../client'
-import { useRouter } from 'next/router'
-
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-  const [authenticatedState, setAuthenticatedState] = useState('not-authenticated')
-  useEffect(() => {
-    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
-      handleAuthChange(event, session)
-      if (event === 'SIGNED_IN') {
-        setAuthenticatedState('authenticated')
-        router.push('/dashboard')
-      }
-      if (event === 'SIGNED_OUT') {
-        setAuthenticatedState('not-authenticated')
-      }
-    })
-    checkUser()
-    return () => {
-      authListener.unsubscribe()
-    }
-  }, [])
-  async function checkUser() {
-    const user = await supabase.auth.user()
-    if (user) {
-      setAuthenticatedState('authenticated')
-    }
-  }
-  async function handleAuthChange(event, session) {
-    await fetch('/api/auth', {
-      method: 'POST',
-      headers: new Headers({ 'Content-Type': 'application/json' }),
-      credentials: 'same-origin',
-      body: JSON.stringify({ event, session }),
-    })
-  }
-  return (
-
-    <div>
-
-      <Component {...pageProps} />
-    </div>
-  )
-}
-
-const navStyle = {
-  margin: 20
-}
-
-const linkStyle = {
-  marginRight: 10
+  return <Component {...pageProps} />
 }
 
 export default MyApp
--- pages/payment/notfound.js
+++
@@ -1,7 +0,0 @@
-const storeNotFound = () => {
-    return(
-        <h1>This Store does not exist.</h1>
-    )
-}
-
-export default storeNotFound;
